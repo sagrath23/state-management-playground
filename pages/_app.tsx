9dce78db10fd64cf4a937fc0b6e61567ab886ddc
Steven Augusto Villegas <Steven.VillegasC@perficient.com>
2021-05-25 21:28:12 -0500

    add redux-toolkit setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,15 @@ import React from 'react';
 // Definitions
 import { AppProps } from 'next/app';
 
+// redux-toolkit
+import { store } from '../src/vendors/redux-toolkit';
+import { Provider } from 'react-redux'
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <Provider store={store}>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 };
 
---
+++ pages/characters/index.tsx
@@ -0,0 +1,10 @@
+
+import { FC } from 'react';
+
+const CharacterList: FC = () => (
+  <ul>
+    <li>Some characters</li>
+  </ul>
+); 
+
+export default CharacterList;
